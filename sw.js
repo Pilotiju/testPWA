b522871d4e224af8482cf8c220819acd1ae1c701
Pilotiju <bt1.vietanh@gmail.com>
2026-06-20 15:14:36 +0200

    sdgioujsdfiuoimsudfhxiusmhd

--- sw.js
+++ sw.js
@@ -15,3 +15,17 @@ self.addEventListener("install", (event) => {
     ]),
   );
 });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version if available
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // Return offline fallback if needed
+        return caches.match("https://pilotiju.github.io/testPWA/index.html");
+      })
+  );
+});
